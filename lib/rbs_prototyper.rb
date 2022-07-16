9bc71271cceffec27f51748aef8a7a9d01880b8c
yamashun <yshunske@gmail.com>
2022-07-16 19:07:30 +0900

    choose command rbs or typeprof

--- lib/rbs_prototyper.rb
+++ lib/rbs_prototyper.rb
@@ -7,32 +7,39 @@ module RbsPrototyper
   class Error < StandardError; end
   # Your code goes here...
 
-  def self.generate(target_path:, output_dir: 'sig')
+  def self.generate(target_path:, output_dir: 'sig', command: 'rbs')
     raise Error, '指定したファイルまたはディレクトリが存在しません' if !File.file?(target_path) && !File::directory?(target_path)
     raise Error, '.rb ファイルを指定してください' if File.file?(target_path) && !target_path.end_with?('.rb')
+    raise Error, '不正なコマンドです' unless PROTOTYPE_COMMANDS.include?(command)
 
     if File::directory?(target_path)
-      generate_rbs_recursively(target_path, output_dir)
+      generate_rbs_recursively(target_path, output_dir, command)
     else
-      generate_rbs(target_path, output_dir)
+      generate_rbs(target_path, output_dir, command)
     end
   end
 
   private
 
-  def self.generate_rbs_recursively(target_path, output_dir)
+  PROTOTYPE_COMMANDS = {
+    'rbs' => 'rbs prototype rb',
+    'typeprof' => 'typeprof'
+  }
+
+  def self.generate_rbs_recursively(target_path, output_dir, command)
     Dir.glob('**/*', File::FNM_DOTMATCH, base: target_path).each do |file|
       if file.end_with?('.rb')
         file_path = File.join(target_path, file)
         generate_rbs_path = "#{output_dir}/#{file}"
         FileUtils.mkdir_p(File.dirname(generate_rbs_path)) if file.include?('/')
-        `typeprof #{file_path} > #{generate_rbs_path}s`
+        `#{PROTOTYPE_COMMANDS[command]} #{file_path} > #{generate_rbs_path}s`
       end
     end
   end
 
-  def self.generate_rbs(target_path, output_dir)
+  def self.generate_rbs(target_path, output_dir, command)
     generate_rbs_path = "#{output_dir}/#{File.basename(target_path)}"
-    `typeprof #{target_path} > #{generate_rbs_path}s`
+    FileUtils.mkdir_p(File.dirname(generate_rbs_path))
+    `#{PROTOTYPE_COMMANDS[command]} #{target_path} > #{generate_rbs_path}s`
   end
 end
